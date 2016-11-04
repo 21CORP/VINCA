f36c4043fd877441e881698090be97f21a5cb053
Thomas Bech Madsen <thomasbm@live.dk>
2016-11-04 10:56:13 +0100

    Added logic for load button, create still needs an activity to start

--- app/src/main/java/com/example/a21corp/vinca/MainMenu.java
+++ app/src/main/java/com/example/a21corp/vinca/MainMenu.java
@@ -1,13 +1,34 @@
 package com.example.a21corp.vinca;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainMenu extends AppCompatActivity {
+public class MainMenu extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        Button create = (Button) findViewById(R.id.newProjectButton);
+        Button load = (Button) findViewById(R.id.loadMenuButton);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.newProjectButton:{
+                //startActivity(new Intent(this, CreateMenu));
+                break;
+            }
+            case R.id.loadMenuButton:{
+                startActivity(new Intent(this, LoadActivity.class));
+                break;
+            }
+
+        }
     }
 }

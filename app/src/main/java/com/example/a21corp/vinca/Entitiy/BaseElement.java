f315c8c969db21f40b511638f10b600784ba779e
Yanki Muslu <yamuslu@gmail.com>
2016-11-12 04:30:17 +0100

    Editor layout and VINCA symbols added

--- app/src/main/java/com/example/a21corp/vinca/Entitiy/BaseElement.java
+++ app/src/main/java/com/example/a21corp/vinca/Entitiy/BaseElement.java
@@ -1,5 +1,7 @@
 package com.example.a21corp.vinca.Entitiy;
 
+import android.view.View;
+
 /**
  * Created by ymuslu on 12-11-2016.
  */
@@ -7,4 +9,6 @@ package com.example.a21corp.vinca.Entitiy;
 public class BaseElement {
     public int icon;
     public int elementID;
+    //TODO: 1: Rename, 2: Make custom view
+    public View view;
 }

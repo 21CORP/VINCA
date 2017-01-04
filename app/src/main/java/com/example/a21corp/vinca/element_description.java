bd6aa059390c2bf41b0d416927f8394f0ee92a55
Thomas Bech Madsen <thomasbm@live.dk>
2017-01-04 11:22:04 +0100

    element_description now edits element data

--- app/src/main/java/com/example/a21corp/vinca/element_description.java
+++ app/src/main/java/com/example/a21corp/vinca/element_description.java
@@ -1,27 +1,69 @@
 package com.example.a21corp.vinca;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.a21corp.vinca.Editor.EditorActivity;
+import com.example.a21corp.vinca.elements.VincaElement;
+import com.example.a21corp.vinca.vincaviews.VincaElementView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class element_description extends Fragment {
+public class element_description extends DialogFragment{
+
+    VincaElement workElement;
+    TextView titleTV;
+    TextView descTV;
 
     public element_description() {
         // Required empty public constructor
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_element_description, container, false);
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(),R.style.AlertDialogCustom);
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        View promptView = inflater.inflate(R.layout.element_description, null);
+        titleTV = (TextView) promptView.findViewById(R.id.TitleTextView);
+        descTV = (TextView) promptView.findViewById(R.id.DescriptionTextView);
+
+        titleTV.setText(workElement.title);
+        descTV.setText(workElement.description);
+
+        builder.setView(promptView);
+        builder
+                .setPositiveButton("Save", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        workElement.title = titleTV.getText().toString();
+                        workElement.description = descTV.getText().toString();
+                    }
+                })
+
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                    }
+
+                });
+
+        return builder.create();
+    }
+
+    public void setElement(VincaElement element){
+        workElement = element;
     }
 }

2904fc4c6074570eeeeee7246759ba8948f1cd86
Thomas Bech Madsen <thomasbm@live.dk>
2017-01-04 15:10:18 +0100

    Added methods to MoveCommand

--- app/src/main/java/com/example/a21corp/vinca/HistoryManagement/MoveCommand.java
+++ app/src/main/java/com/example/a21corp/vinca/HistoryManagement/MoveCommand.java
@@ -1,12 +1,23 @@
 package com.example.a21corp.vinca.HistoryManagement;
 
+import com.example.a21corp.vinca.Editor.VincaViewManager;
+import com.example.a21corp.vinca.vincaviews.VincaElementView;
+
 /**
  * Created by Thomas on 04-01-2017.
  */
 
 public class MoveCommand implements Command {
-    @Override
-    public void execute() {
+    VincaElementView view;
+    VincaViewManager manager;
 
+    public MoveCommand(VincaElementView view, VincaViewManager manager){
+        this.view = view;
+        this.manager = manager;
+    }
+
+    @Override
+    public void execute(){
+        manager.addElement(view);
     }
 }

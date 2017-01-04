bd80e7c44a515e1a7b0a1ebd6ebd5663da0a9aac
Thomas Bech Madsen <thomasbm@live.dk>
2017-01-04 15:09:16 +0100

    Added methods to CreateCommand

--- app/src/main/java/com/example/a21corp/vinca/HistoryManagement/CreateCommand.java
+++ app/src/main/java/com/example/a21corp/vinca/HistoryManagement/CreateCommand.java
@@ -1,12 +1,24 @@
 package com.example.a21corp.vinca.HistoryManagement;
 
+import com.example.a21corp.vinca.Editor.VincaViewManager;
+import com.example.a21corp.vinca.vincaviews.VincaElementView;
+
 /**
  * Created by Thomas on 04-01-2017.
  */
 
 public class CreateCommand implements Command{
-    @Override
-    public void execute() {
+    VincaElementView view;
+    VincaViewManager manager;
 
+    public CreateCommand(VincaElementView view, VincaViewManager manager){
+        this.view = view;
+        this.manager = manager;
     }
+
+    @Override
+    public void execute(){
+        manager.addElement(view);
+    }
+
 }

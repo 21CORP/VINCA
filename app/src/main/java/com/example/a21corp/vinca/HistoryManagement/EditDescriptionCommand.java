938bfe000b557c20ed3086316910736f6b78767a
Thomas Bech Madsen <thomasbm@live.dk>
2017-01-05 09:21:35 +0100

    Finished EditDescriptionCommand

--- app/src/main/java/com/example/a21corp/vinca/HistoryManagement/EditDescriptionCommand.java
+++ app/src/main/java/com/example/a21corp/vinca/HistoryManagement/EditDescriptionCommand.java
@@ -7,21 +7,20 @@ import com.example.a21corp.vinca.vincaviews.VincaElementView;
  * Created by Thomas on 04-01-2017.
  */
 
-public class EditDescriptionCommand {
+public class EditDescriptionCommand implements Command{
     VincaElementView view;
     VincaViewManager manager;
     String title;
     String description;
 
-    public CreateCommand(VincaElementView view, VincaViewManager manager, String title, String description){
+    public EditDescriptionCommand(VincaElementView view, VincaViewManager manager, String title, String description){
         this.view = view;
         this.manager = manager;
-        this.title = title;
-        this.description = description;
     }
 
     @Override
     public void execute(){
-        //TODO - Fix
+        view.element.title = title;
+        view.element.description = description;
     }
 }

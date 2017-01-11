bdcde9eecd9cf955cdb4b0e9ecf069eb8f4d5428
Thomas Bech Madsen <thomasbm@live.dk>
2017-01-11 15:47:28 +0100

    Still puzzled

--- app/src/main/java/com/example/a21corp/vinca/CreateMenuPopUp.java
+++ app/src/main/java/com/example/a21corp/vinca/CreateMenuPopUp.java
@@ -5,8 +5,10 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -93,10 +95,11 @@ Button b1;
 
     public void importFile() {
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setAction(Intent.ACTION_GET_CONTENT);
         intent.setType("*/*"); //TODO change to "*/*.ser" when we know it works
         intent.addCategory(Intent.CATEGORY_OPENABLE);
         try {
-            startActivityForResult(Intent.createChooser(intent, "Select a file to import"), FILE_SELECT_CODE);
+            startActivityForResult(intent, FILE_SELECT_CODE);
         }
         catch(Exception e){
             e.printStackTrace();

97dab222111f2992b06c675fea886fbc360cf582
Hjorthen <Hjorthensebastian@gmail.com>
2017-01-16 21:43:34 +0100

    Finally got the SVG files to cooperate

--- app/src/main/java/com/example/a21corp/vinca/VincatoSVG.java
+++ app/src/main/java/com/example/a21corp/vinca/VincatoSVG.java
@@ -5,4 +5,17 @@ package com.example.a21corp.vinca;
  */
 
 public class VincatoSVG {
+    public static class IconWidth{
+        public float conclusion = 120;
+        public float activity = 290;
+        public float pause = 275;
+        public float method = 222;
+        public float iterationEnd = 135;
+        public float iterationBegin = 135;
+        public float projectBegin = 135;
+        public float projectEnd = 115;
+        public float processBegin = 122;
+        public float processEnd = 120;
+    }
+
 }

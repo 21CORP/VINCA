38cfff80d4ae3bf7f56185659528c053cdbb5745
Thomas Bech Madsen <thomasbm@live.dk>
2017-01-19 14:06:08 +0100

    Added warning message when deleting projects Fixed bug where the next project would be checked for deletion

--- app/src/main/java/com/example/a21corp/vinca/LoadActivity.java
+++ app/src/main/java/com/example/a21corp/vinca/LoadActivity.java
@@ -1,24 +1,30 @@
 package com.example.a21corp.vinca;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
+import android.os.Build;
 import android.support.v4.app.FragmentTransaction;
 import android.app.SearchManager;
 import android.app.SearchableInfo;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.ActionMode;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.a21corp.vinca.AndroidUtilities.FolderAdapter;
@@ -166,19 +172,55 @@ public class LoadActivity extends AppCompatActivity implements OnFileSelectedLis
 
     @Override
     public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
-        int amount = selectedFiles.size();
+        final int amount = selectedFiles.size();
         if(item.getItemId()==R.id.delete_workspace)
         {
-            for (int i = 0; i < selectedFiles.size(); i++)
-            {
-                File c = selectedFiles.get(i);
-                File preview = new File(c.getParentFile() + "/previews" + c.getName().replace(".ser", ".png"));
-                preview.delete();
-                c.delete();
+            final AlertDialog.Builder builder = new AlertDialog.Builder(this,R.style.AlertDialogCustom);
+            LayoutInflater inflater = this.getLayoutInflater();
 
-            }
-            listadapter.FilterDirectory(""); // Refreshes the directory
-            Toast.makeText(this, "Deleted " + amount + " items", Toast.LENGTH_LONG);
+            View promptView = inflater.inflate(R.layout.deletewarning, null);
+            builder.setView(promptView);
+
+            ((TextView)promptView.findViewById(R.id.deleteMessage)).setText("Warning! You are about to delete " + amount + " projects");
+            builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    for (int i = 0; i < selectedFiles.size(); i++)
+                    {
+                        File c = selectedFiles.get(i);
+                        File preview = new File(c.getParentFile() + "/previews" + c.getName().replace(".ser", ".png"));
+                        preview.delete();
+                        c.delete();
+                    }
+                    listadapter.FilterDirectory(""); // Refreshes the directory
+                    Toast.makeText(builder.getContext(), "Deleted " + amount + " items", Toast.LENGTH_LONG);
+                    Intent intent = getIntent();
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    finish();
+                    overridePendingTransition(0,0);
+                    startActivity(intent);
+                }
+            });
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener(){
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                }
+            });
+            final AlertDialog dialog = builder.create();
+
+            dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+                @Override
+                public void onShow(final DialogInterface dialog) {
+                    Button positiveButton = ((AlertDialog)dialog).getButton(DialogInterface.BUTTON_POSITIVE);
+                    Button negativeButton = ((AlertDialog)dialog).getButton(DialogInterface.BUTTON_NEGATIVE);
+                    positiveButton.setTextColor(ContextCompat.getColor(builder.getContext(), R.color.background_material_light_1));
+                    negativeButton.setTextColor(ContextCompat.getColor(builder.getContext(), R.color.cancelColor));
+                }
+            });
+            builder.show();
+
+
             return true;
         }
         return false;
